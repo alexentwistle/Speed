dc06c136c5f7729f877e131022b413e4d21a9e58
Alex Entwistle <alexentwistleseo@gmail.com>
2020-01-15 16:08:20 +0000

    Updated speed.py

---
+++ speed.py
@@ -0,0 +1,16 @@
+# Get URL from user input, and append to Google PageSpeedInsights API URL, and curl (or equivalent). 
+# This will use the requests library for convenience.
+
+import requests
+import json
+
+url = input("What URL would you like to check: ")
+
+params = (
+    ('url', url),
+)
+
+response = requests.get('https://www.googleapis.com/pagespeedonline/v5/runPagespeed', params=params)
+
+json_string = response.json()
+print(json_string)
